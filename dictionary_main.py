247f778ede5e350abde628671e8e7756ba12c6c8
coderuday21 <84789045+coderuday21@users.noreply.github.com>
2021-05-25 20:54:17 +0530

    Create dictionary_main.py
    Dictionary is an unordered collection of items. it's represented as bunch of key:value pairs in which keys should always be unique. Capitalize() function is used to make the first letter of the word capital.

---
+++ dictionary_main.py
@@ -0,0 +1,3 @@
+print("Looking for words, find it in dictionary")
+dictionary={"Immutable":"Unchangable","Mutable":"Changable","Abandon":"To Leave","Integrity":"Honesty","Shabby":"Worn Out"}
+print("Meaning of the word:",dictionary[input("Enter word you are looking for:").capitalize()])
